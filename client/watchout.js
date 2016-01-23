39587ab3cf11926a9168e5d4bc92c08a83337e62
Jonathon Lo <jonathon.lo35@gmail.com>
2016-01-23 12:26:16 -0800

    'added collisions'

--- client/watchout.js
+++ client/watchout.js
@@ -1,20 +1,24 @@
 // start slingin' some d3 here.
-
-
 var gameSize= {
 	height: 800,
-	width: 700,		
-	enemies: 30
+	width: 800,		
+	enemies: 5,
+	r:20
 	}
 
 var currentScore = 0;
 var highScore = 0;
 var collisionCount = 0;
 
+var player = {
+	x: gameSize.width/2,
+	y: gameSize.height/2
+}
+
 var scoreUpdate = function(){
 	d3.select('.scoreboard .current span').text(currentScore);
 	d3.select('.scoreboard .high span').text(highScore);
-	d3.select('.scoraboard .collisions span').text(collisionCount);
+	d3.select('.scoreboard .collisions span').text(collisionCount);
 }
 
 var scoreCounter = function(){
@@ -24,26 +28,37 @@ var scoreCounter = function(){
 	}
 	scoreUpdate()
 }
-setInterval(scoreCounter,500);
+setInterval(scoreCounter,100);
 
-
+// Add string px 
 var addPx = function(number){
 	return number + 'px';
 }
-
+//create board using gameSize parameters
 var board = d3.select('.board').style({
 	width: addPx(gameSize.width),
 	height: addPx(gameSize.height)
 });
-
+//randomized Y coordinates
 var randomY = function(){
 	return addPx (Math.floor(Math.random() * gameSize.height))
 };
-
+//randomized Y coordinates
 var randomX = function(){
 	return addPx (Math.floor(Math.random() * gameSize.width))
 };
 
+d3.select('.player')
+ 
+ 	.style({
+	top: addPx(player.y),
+	left: addPx(player.x),
+	width: addPx(gameSize.r),
+	height: addPx(gameSize.r)
+
+})//.call(drag);
+ 	
+//creating enemies and placing them on the board
 var asteroids = board.selectAll('.asteroids')
 	.data(d3.range(gameSize.enemies))
 	.enter().append('div')
@@ -57,14 +72,10 @@ var asteroids = board.selectAll('.asteroids')
 		return randomX();
 	})
 	.style({
-		height: '30px',
-		width: '30px'
+		width: addPx(gameSize.r),
+		height: addPx(gameSize.r),
 	});
 
-//var distance=asteroids.
-//var xDiff=asteroids
-//var yDiff=
-//
 var transition = function(){
 	asteroids.transition().style('top',function(){
 		return randomY();}).style('left',function(){
@@ -72,35 +83,56 @@ var transition = function(){
 		}).duration(1000)
 
 };
-var intervalID=window.setInterval(transition,1000);
+setInterval(transition,1000);
 
-var player = d3.select('.board')
-	.append('svg').attr('class','player')
-	.attr('width',50).attr('height',50)
-	.append('circle').attr('cx',25).attr('cy',25).attr('r',10)
-	.style('fill','purple')
-	
+// d3.selectAll('.player').on('mousedown', function(){
+// 	var player = d3.select(this)
+// 	 .classed('active,' true);
 
-//var distance
+// 	 var track = d3.select(window)
+// 	  .on('mousemove', mousemove)
+// 	  .on('mouseup', mouseup);
 
-//var collision = asteroids.each(function(d){
-//	var x=d3.select(this).style('top');
-//	var y=d3.select(this).style('left');
-//
-//	var x1 = x - player.x1;
-//	var y1 = y - player.y1;
-//})
+// 	  function mouseup(){
+// 	  	player.classed('active', false);
+// 	  	track.on('mousemove'. null).on('mouseup', null);
 
-// setInterval(transition,1000);
-// var enemyData=_.range(0,gameSize.enemies).map(function(i){
-// 	return {
-// 		id:i,
-// 		x:Math.random()*100,
-// 		y.Math.random()*100
-// 	}
+// 	  }
 // })
+var move = function(){
+//     
+var dragTarget = d3.select(this);
+//     
+   dragTarget
+       .attr("cx", function(){return d3.event.dx + parseInt(dragTarget.style('top'),10)})
+       .attr("cy", function(){return d3.event.dy + parseInt(dragTarget.style('left'),10)});
+};
+var drag=d3.behavior.drag().on("drag", move);
+
+d3.select('.player').call(drag)
 
 
 
+var collisions = function(){
+	var collision = false;
+
+	asteroids.each(function(){
+		var cx = this.offsetLeft + gameSize.r;
+		var cy = this.offsetTop + gameSize.r;
+
+		var x = cx - player.x;
+		var y = cy - player.y;
+
+		if(Math.sqrt(x*x + y*y) < gameSize.r){
+			collision = true;
+		}
+	})
+	if(collision){
+		currentScore = 0;
+		collisionCount = collisionCount +1;
+	}
+}
+
+d3.timer(collisions)
 
 	
